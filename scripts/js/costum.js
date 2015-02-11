d07d24944ff217b46997d4fe7347cee7ea176c7c
luxfinis <luxfinis@ruggedinbox.com>
2015-02-11 23:53:36 +0100

    added login function
    Signed-off-by: luxfinis <luxfinis@ruggedinbox.com>

--- scripts/js/costum.js
+++ scripts/js/costum.js
@@ -1,18 +1,8 @@
+function Login(e){
 
+	var mailaddress = document.getElementById('loginMailAddress').value;
+	var password = document.getElementById('loginPassword').value;
 
-function ShowTotalArticleContent(e){
+	alert("address: " + mailaddress + "    " + " pw: " + password);
 
-	alert("I am an alert box!");
-/**
-
-	// singlePageLink
-	var MyDiv1 = document.getElementById('DIV1');
-	var MyDiv2 = document.getElementById('DIV2');
-	MyDiv2.innerHTML = MyDiv1.innerHTML;
-**/
-
-	//window.url = singlepage;
 };
-
-								
-							
