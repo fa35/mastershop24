688acc9ab0fecc598b1d778c7752ee843dceb731
Michael Große <mic.grosse+github@googlemail.com>
2015-02-11 22:18:10 +0100

    Only offer actually used categories

--- scripts/php/navigation.php
+++ scripts/php/navigation.php
@@ -8,17 +8,8 @@ class navigation {
        $doc .= '<div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
             <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Elektro</a></li>
-            <li><a href="#">Kleidung</a></li>
-            <li><a href="#">Sport</a></li>
-            <li><a href="#">Motorsport</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nahrung</a></li>
-            <li><a href="">Print</a></li>
-            <li><a href="">Spielzeug</a></li>
-            <li><a href="">Kleintiere</a></li>
-            <li><a href="">Haushalt</a></li>
+            <li><a href="?category=1">Literatur</a></li>
+            <li><a href="?category=2">Haushaltsgeräte</a></li>
           </ul>
         </div>';
     }
