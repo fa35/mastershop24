7416a7fe4bfa361326e50ee02523d75bf0428d32
luxfinis <luxfinis@ruggedinbox.com>
2015-02-12 00:12:50 +0100

    added register function
    Signed-off-by: luxfinis <luxfinis@ruggedinbox.com>

--- scripts/js/costum.js
+++ scripts/js/costum.js
@@ -6,3 +6,19 @@ function Login(e){
 	alert("address: " + mailaddress + "    " + " pw: " + password);
 
 };
+
+
+
+function Register(e){
+
+	var lastname = document.getElementById('regLastName').value;
+	var firstname = document.getElementById('regFirstName').value;
+	var street = document.getElementById('regStreet').value;
+	var housenr = document.getElementById('regHouseNr').value;
+	var postcode = document.getElementById('regPcode').value;
+	var place = document.getElementById('regPlace').value;
+	var regMailAdd = document.getElementById('regMailAddress').value;
+	var regPw = document.getElementById('regPasswd').value;
+
+	alert("lastname: " + lastname + " firstname: " + firstname + " street: " +street + " housenr: " + housenr + " postcode: " + postcode + " place: " + place + " mailaddress: " + regMailAdd + " password: " + regPw);
+}
