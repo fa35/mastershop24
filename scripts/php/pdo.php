05768f470dbfcd86af2f9357c05dd34dd675a312
Michael Große <mic.grosse+github@googlemail.com>
2015-02-11 21:40:09 +0100

    Fix missing $conn variable in pdo.php

--- scripts/php/pdo.php
+++ scripts/php/pdo.php
@@ -5,34 +5,35 @@ class dbconnect {
     var $username = "root";
     var $password = "2h1fxL,Jtx";
     var $dbname = "mydb";
+    var $conn;
 
     function dbconnect(){}
 
     function getConnection(){
         try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
             // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully\n";
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //echo "Connected successfully\n";
         }
         catch(PDOException $e){
             echo "Connection failed: " . $e->getMessage();
         }
-        return $conn;
+        return $this->conn;
     }
 
     function getQueryResults($sqlQuery){
 
-        $sqlQuery = "SELECT * FROM Artikel";
+        //$sqlQuery = "SELECT * FROM Artikel";
 
         $result = Array();
-        foreach ($conn->query($sqlQuery) as $row) {
+        foreach ($this->conn->query($sqlQuery) as $row) {
             $result[] = $row;
         }
         return $result;
     }
 
     function closeConnection(){
-        $conn->close();
+        $this->conn->close();
     }
 }
