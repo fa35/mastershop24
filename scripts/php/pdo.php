4a909456903da401762a3b7d410d65048c6b236d
Michael Große <mic.grosse+github@googlemail.com>
2015-02-11 20:43:37 +0100

    Create class to connect to database

--- scripts/php/pdo.php
+++ scripts/php/pdo.php
@@ -1,25 +1,38 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "2h1fxL,Jtx";
-$dbname = "mydb";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully\n";
+class dbconnect {
+    var $servername = "localhost";
+    var $username = "root";
+    var $password = "2h1fxL,Jtx";
+    var $dbname = "mydb";
+
+    function dbconnect(){}
+
+    function getConnection(){
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully\n";
+        }
+        catch(PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+        }
+        return $conn;
     }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
+
+    function getQueryResults($sqlQuery){
+
+        $sqlQuery = "SELECT * FROM Artikel";
+
+        $result = Array();
+        foreach ($conn->query($sqlQuery) as $row) {
+            $result[] = $row;
+        }
+        return $result;
     }
 
-$sql = "SELECT * FROM Artikel";
-
-foreach ($conn->query($sql) as $row) {
-        print $row['Titel'] . "\n";
+    function closeConnection(){
+        $conn->close();
     }
-
-
-$conn->close();
+}
