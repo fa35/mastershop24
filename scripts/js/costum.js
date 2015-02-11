02980985ded9ecac6fe2c826313796b0dc2aecff
luxfinis <luxfinis@ruggedinbox.com>
2015-02-11 22:41:29 +0100

    created / changed examples
    Signed-off-by: luxfinis <luxfinis@ruggedinbox.com>

---
+++ scripts/js/costum.js
@@ -0,0 +1,18 @@
+
+
+function ShowTotalArticleContent(e){
+
+	alert("I am an alert box!");
+/**
+
+	// singlePageLink
+	var MyDiv1 = document.getElementById('DIV1');
+	var MyDiv2 = document.getElementById('DIV2');
+	MyDiv2.innerHTML = MyDiv1.innerHTML;
+**/
+
+	//window.url = singlepage;
+};
+
+								
+							
