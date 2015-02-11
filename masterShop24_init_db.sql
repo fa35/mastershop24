d9ab33709f78d01ea5dadb40c7b60523bdcf88df
luxfinis <luxfinis@ruggedinbox.com>
2015-02-11 19:44:54 +0100

    changed db name to mastershop25, added field for picture link, change some fields which should never by null

--- masterShop24_init_db.sql
+++ masterShop24_init_db.sql
@@ -2,101 +2,102 @@
 -- SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 -- SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `mastershop24` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `mastershop24` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Nutzer`
+-- Table `mastershop24`.`Nutzer`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Nutzer` (
-  `idNutzer` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(150) NULL,
-  `Vorname` VARCHAR(150) NULL,
-  `Strasse` VARCHAR(100) NULL,
-  `HausNr` VARCHAR(10) NULL,
-  `PLZ` INT NULL,
-  `Ort` VARCHAR(150) NULL,
-  `Email` VARCHAR(150) NULL,
-  `Passwort` VARCHAR(150) NULL,
-  `IsAdmin` BIT NULL,
-  PRIMARY KEY (`idNutzer`))
+CREATE TABLE IF NOT EXISTS `mastershop24`.`Nutzer` (
+  `IdNutzer` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(150) NOT NULL,
+  `Vorname` VARCHAR(150) NOT NULL,
+  `Strasse` VARCHAR(100) NOT NULL,
+  `HausNr` VARCHAR(10) NOT NULL,
+  `PLZ` INT NOT NULL,
+  `Ort` VARCHAR(150) NOT NULL,
+  `Email` VARCHAR(150) NOT NULL,
+  `Passwort` VARCHAR(150) NOT NULL,
+  `IsAdmin` BIT NOT NULL,
+  PRIMARY KEY (`IdNutzer`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Bestellungen`
+-- Table `mastershop24`.`Bestellung`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Bestellungen` (
-  `idBestellungen` INT NOT NULL AUTO_INCREMENT,
-  `idNutzer` VARCHAR(150) NULL,
-  `Datum` DATETIME NULL,
+CREATE TABLE IF NOT EXISTS `mastershop24`.`Bestellung` (
+  `IdBestellung` INT NOT NULL AUTO_INCREMENT,
+  `IdNutzer` VARCHAR(150) NOT NULL,
+  `Datum` DATETIME NOT NULL,
   `ZahlArt` VARCHAR(150) NULL,
   `R_Name` VARCHAR(150) NULL,
   `R_Vorname` VARCHAR(150) NULL,
   `R_Strasse_Nr` VARCHAR(250) NULL,
   `R_PLZ_ORT` VARCHAR(250) NULL,
-  `Bezahlt` BIT NULL,
-  PRIMARY KEY (`idBestellungen`))
+  `Bezahlt` BIT NOT NULL,
+  PRIMARY KEY (`IdBestellung`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`BestellungsDetails`
+-- Table `mastershop24`.`BestellungsDetails`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`BestellungsDetails` (
-  `idBestellungsDetails` INT NOT NULL AUTO_INCREMENT,
-  `idBestellung` INT NULL,
-  `ArtikelName` VARCHAR(250) NULL,
-  `NettoPreis` DECIMAL NULL,
-  `MwSt` DECIMAL NULL,
-  `Anzahl` INT NULL,
-  PRIMARY KEY (`idBestellungsDetails`))
+CREATE TABLE IF NOT EXISTS `mastershop24`.`BestellungsDetails` (
+  `IdBestellungsDetails` INT NOT NULL AUTO_INCREMENT,
+  `IdBestellung` INT NOT NULL,
+  `ArtikelName` VARCHAR(250) NOT NULL,
+  `NettoPreis` DECIMAL NOT NULL,
+  `MwSt` DECIMAL NOT NULL,
+  `Anzahl` INT NOT NULL,
+  PRIMARY KEY (`IdBestellungsDetails`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Steuersaetze`
+-- Table `mastershop24`.`Steuersatz`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Steuersaetze` (
-  `idSteuersaetze` INT NOT NULL AUTO_INCREMENT,
-  `Titel` VARCHAR(150) NULL,
-  `Steuersatz` DECIMAL NULL,
-  PRIMARY KEY (`idSteuersaetze`))
+CREATE TABLE IF NOT EXISTS `mastershop24`.`Steuersatz` (
+  `IdSteuersatz` INT NOT NULL AUTO_INCREMENT,
+  `Titel` VARCHAR(150) NOT NULL,
+  `Steuersatz` DECIMAL NOT NULL,
+  PRIMARY KEY (`IdSteuersatz`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Artikel`
+-- Table `mastershop24`.`Artikel`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Artikel` (
-  `idArtikel` INT NOT NULL AUTO_INCREMENT,
-  `Titel` VARCHAR(250) NULL,
-  `idHauptgruppe` INT NULL,
-  `NettoPreis` DECIMAL NULL,
-  `MwStSatz` DECIMAL NULL,
+CREATE TABLE IF NOT EXISTS `mastershop24`.`Artikel` (
+  `IdArtikel` INT NOT NULL AUTO_INCREMENT,
+  `Titel` VARCHAR(250) NOT NULL,
+  `IdHauptgruppe` INT NOT NULL,
+  `IdUntergruppe` INT NULL,
+  `NettoPreis` DECIMAL NOT NULL,
+  `MwStSatz` DECIMAL NOT NULL,
   `Beschreibung` VARCHAR(250) NULL,
-  `idUntergruppe` INT NULL,
-  PRIMARY KEY (`idArtikel`))
+  `BildLink` VARCHAR(250) NULL,
+  PRIMARY KEY (`IdArtikel`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Hauptgruppe`
+-- Table `mastershop24`.`Hauptgruppe`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Hauptgruppe` (
-  `idHauptgruppe` INT NOT NULL AUTO_INCREMENT,
-  `Titel` VARCHAR(150) NULL,
-  PRIMARY KEY (`idHauptgruppe`))
+CREATE TABLE IF NOT EXISTS `mastershop24`.`Hauptgruppe` (
+  `IdHauptgruppe` INT NOT NULL AUTO_INCREMENT,
+  `Titel` VARCHAR(150) NOT NULL,
+  PRIMARY KEY (`IdHauptgruppe`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Untergruppe`
+-- Table `mastershop24`.`Untergruppe`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Untergruppe` (
-  `idUntergruppe` INT NOT NULL AUTO_INCREMENT,
-  `Titel` VARCHAR(150) NULL,
-  PRIMARY KEY (`idUntergruppe`))
+CREATE TABLE IF NOT EXISTS `mastershop24`.`Untergruppe` (
+  `IdUntergruppe` INT NOT NULL AUTO_INCREMENT,
+  `Titel` VARCHAR(150) NOT NULL,
+  PRIMARY KEY (`IdUntergruppe`))
 ENGINE = InnoDB;
 
 
